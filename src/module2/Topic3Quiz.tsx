3e78b6cc9b82031871cb304137c06957250e3d8f
Kacey Dove <kjdove@udel.edu>
2026-05-07 14:47:17 -0400

    localstorage for m2t3 mcq

--- src/module2/Topic3Quiz.tsx
+++ src/module2/Topic3Quiz.tsx
@@ -10,7 +10,40 @@ export function Topic3Quiz(): JSX.Element {
     const [currentQInd, setCurrentQInd] = useState<number>(0);
     const currentQuestion = allQuestions[currentQInd];
     const [currentAInd, setCurrentAInd] = useState<number>(0);
+    const [studentAnswers, setSA] = useState<Record<string, string>>({});
+    
+    const handleAnswerChange = (questionId: string, answer: string) => {
+        setSA((prev) => ({
+            ...prev,
+            [questionId]: answer
+        }));
+    }
 
+    const handleSubmit = () => {
+        const studentAnswer = studentAnswers[currentQuestion.id]|| "";
+
+        let correctAnswer;
+        if(currentAInd < 6){
+            correctAnswer = topic3MCQAnswers[currentAInd].correctId;
+        }
+
+        console.log("student: ", studentAnswer);
+        console.log("correct: ", correctAnswer);
+
+        const isCorrect = studentAnswer === correctAnswer;
+        
+        const savedData = JSON.parse(localStorage.getItem("module2topic3") || "{}");
+        savedData[currentQuestion.id] = {
+            studentAnswer,
+            isCorrect
+        };
+
+        localStorage.setItem(
+            "module2topic3",
+            JSON.stringify(savedData)
+        );
+        
+    }//end to handleSubmit
     return (
         <div className="t3-container">
             <div className="question-list">
@@ -35,13 +68,22 @@ export function Topic3Quiz(): JSX.Element {
                     {/**if currentAInd is in range of MCQ questions render this, else render the other possible question type answers */}
                     {currentAInd < 6 && topic3MCQAnswers[currentAInd].options.map((option) => (
                         <div key={option.textId} className="answer-option">
-                            <input type="radio" id={option.textId} name="answer" value={option.textId} />
+                            <input
+                                type="radio"
+                                id={option.textId}
+                                name={currentQuestion.id}
+                                value={option.textId}
+                                checked={studentAnswers[currentQuestion.id] === option.textId}
+                                onChange={(e) =>
+                                    handleAnswerChange(currentQuestion.id, e.target.value)
+                                }
+                            />
                             <label htmlFor={option.textId}>{option.text}</label>
                         </div>
                     ))}
                     {currentAInd >= 6 && <T3Code questionId={currentQuestion.id}/>}
                 </div>
-                <button className="submit-button">Submit</button>
+                <button onClick={handleSubmit}className="submit-button">Submit</button>
             </div>
         </div>
     )
